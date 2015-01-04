381a5be7e2cf28bd4fa52f814cb9974f70097616
Ivan Garavito <ivangaravito@gmail.com>
2015-01-04 14:27:17 -0600

    Move functions to common.js
    Add debug mode

---
+++ common.js
@@ -0,0 +1,78 @@
+var fs = require('fs')
+
+var debugMode = false
+	, each = Array.prototype.forEach
+	, helpFn
+
+module.exports.debug = function () {
+	if (debugMode === false) return
+
+	var args = ['Debug:']
+	each.call(arguments, function (arg) {
+		args.push(arg)
+	})
+	console.log.apply(console, args)
+}
+
+module.exports.error = function (errno, msg) {
+	console.log('Error: ' + msg)
+	if (helpFn) helpFn()
+	process.exit(errno)
+}
+
+module.exports.loadSettings = function () {
+	var settings = {}
+		, keys = ['http', 'https', 'same']
+		, obj = {}
+		, props
+	console.log('Loading settings...')
+
+	try {
+		settings = fs.readFileSync('./settings.json')
+		settings = JSON.parse(settings)
+		props = Object.getOwnPropertyNames(settings)
+	} catch (e) {
+		return false
+	}
+
+	if (props === 0) {
+		return false
+	}
+
+	keys.forEach(function (key) {
+		obj[key] = settings[key]
+	})
+	return obj
+}
+
+module.exports.saveSettings = function (options) {
+	var settings = {}
+		, keys = ['http', 'https', 'same']
+	console.log('Saving settings...')
+
+	keys.forEach(function (key) {
+		settings[key] = options[key]
+	})
+	console.log('settings=', JSON.stringify(settings))
+
+	fs.writeFile('./settings.json', JSON.stringify(settings), function (err) {
+		if (err) {
+			console.log('Warning: Cannot write settings to settings.json')
+		} else {
+			console.log('Settings saved successfuly')
+		}
+	})
+}
+
+module.exports.setDebug = function (debug) {
+	debugMode = !!debug
+}
+
+module.exports.setHelpFn = function (fn) {
+	helpFn = fn
+}
+
+module.exports.urlValidator = function (theUrl) {
+	var reUrl = /^https?:\/\/(?:(\w+)(?:[:](\w+))?[@])?([A-Za-z0-9-]+(?:[.][A-Za-z0-9-]+)*|(\d{1,3}(?:[.]\d{1,3}){3}))[:](\d+)$/
+	return reUrl.exec(theUrl) === null? false: theUrl
+}
--- index.js
+++ index.js
@@ -1,116 +1,52 @@
 var program = require('commander')
-	, pkg = require('./package.json')
-	, url = require('url')
-	, fs = require('fs')
 
-var urlValidator = function (theUrl) {
-	var reUrl = /^https?:\/\/(?:(\w+)(?:[:](\w+))?[@])?([A-Za-z0-9-]+(?:[.][A-Za-z0-9-]+)*|(\d{1,3}(?:[.]\d{1,3}){3}))[:](\d+)$/
-	return reUrl.exec(theUrl) === null? false: theUrl
-}
-
-var loadSettings = function () {
-	var settings = {}
-		, keys = ['http', 'https', 'same']
-		, obj = {}
-		, props
-	console.log('Loading settings...')
-
-	try {
-		settings = fs.readFileSync('./settings.json')
-		settings = JSON.parse(settings)
-		props = Object.getOwnPropertyNames(settings)
-	} catch (e) {
-		return false
-	}
-
-	if (props === 0) {
-		return false
-	}
-
-	keys.forEach(function (key) {
-		obj[key] = settings[key]
-	})
-	return obj
-}
-
-var saveSettings = function (options) {
-	var settings = {}
-		, keys = ['http', 'https', 'same']
-	console.log('Saving settings...')
-
-	keys.forEach(function (key) {
-		settings[key] = options[key]
-	})
-	console.log('settings=', JSON.stringify(settings))
-
-	fs.writeFile('./settings.json', JSON.stringify(settings), function (err) {
-		if (err) {
-			console.log('Warning: Cannot write settings to settings.json')
-		} else {
-			console.log('Settings saved successfuly')
-		}
-	})
-}
+var pkg = require('./package.json')
+	, common = require('./common.js')
 
 program
   .version(pkg.version)
   .description(pkg.description)
 
 program
+  .option('--debug', 'output debbuging messages')
+
+program
 	.command('setup')
 	.description('setup configuration for developing behind the proxy')
-  .option('--http <url>', 'set url for http proxy (e.g. http://myproxy.com:8080)', urlValidator)
-  .option('--https <url>', 'set url for https proxy (e.g. https://me:mypass@myproxy.com:443', urlValidator)
+  .option('--http <url>', 'set url for http proxy (e.g. http://myproxy.com:8080)', common.urlValidator)
+  .option('--https <url>', 'set url for https proxy (e.g. https://me:mypass@myproxy.com:443', common.urlValidator)
   .option('--same', 'use the same http proxy for https')
   .option('--save', 'save settings')
 	.action(function (options) {
-		console.log('http=', options.http)
-		console.log('https=', options.https)
-		console.log('same=', options.same)
-		console.log('save=', options.save)
+		var self = this
 
-		if (options.http === false) {
-			console.log('Error: Bad http proxy url defined')
-			this.outputHelp()
-			process.exit(1)
-		}
+		common.setHelpFn(function () {
+			self.outputHelp()
+		})
+		common.setDebug(options.parent.debug)
 
-		if (options.https === false) {
-			console.log('Error: Bad https proxy url defined')
-			this.outputHelp()
-			process.exit(1)
-		}
+		if (options.http === false) common.error(1, 'Bad http proxy url defined')
+		if (options.https === false) common.error(1, 'Bad https proxy url defined')
 
-		if (options.save === true && options.http === undefined && options.https === undefined) {
-			console.log('Error: Cannot save without http or https definition')
-			this.outputHelp()
-			process.exit(2)
-		}
-
+		if (options.save === true && options.http === undefined && options.https === undefined) common.error(2, 'Cannot save without http or https definition')
 		if (options.http === undefined && options.https === undefined) {
-			var opts = loadSettings()
-			if (opts === false) {
-				console.log('Error: No configuration saved previously')
-				this.outputHelp()
-				process.exit(3)
-			}
-			console.log('Options loaded:', opts)
+			var opts = common.loadSettings()
+			if (opts === false) common.error(3, 'No configuration saved previously')
+			common.debug('Options loaded:', opts)
 		} else if(options.save) {
-			saveSettings(options)
+			common.saveSettings(options)
 		}
 
 		if (options.same === true) {
 			if (options.http !== undefined && options.https === undefined) {
 				options.https = options.http
 			} else {
-				console.log('Error: Cannot use the same http proxy for https if http is not defined')
-				this.outputHelp()
-				process.exit(4)
+				common.error(4, 'Cannot use the same http proxy for https if http is not defined')
 			}
 		}
 
 		options.home = process.env.HOME
-		console.log('HOME=', options.home)
+		common.debug('HOME=', options.home)
 
 		// TODO: setup
 	})
