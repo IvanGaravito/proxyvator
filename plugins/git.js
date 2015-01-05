fbcf91e23b13743a62cbf8eb1703f1960fd09acc
Ivan Garavito <ivangaravito@gmail.com>
2015-01-04 19:15:07 -0600

    git: add handler for error support

--- plugins/git.js
+++ plugins/git.js
@@ -1,10 +1,21 @@
 var name = module.exports.name = 'git'
 	, exec = require('child_process').exec
+
+var handler = function (error) {
+	if (error !== null) {
+		if (error.message.match(/git --help/) !== null) {
+			console.log('[' + name + '] Returned error code ' + error.code + ': ' + error.message)
+		} else if (error.code === 1) {
+			console.log('[' + name + '] Seems git is not installed or not accesible.')
+		}
+	}
+}
+
 module.exports.clear = function () {
 	console.log(name + '::clear')
 	try {
-		exec('git config --global --unset http.proxy')
-		exec('git config --global --unset https.proxy')
+		exec('git config --global --unset http.proxy', handler)
+		exec('git config --global --unset https.proxy', handler)
 	} catch (e) {
 	}
 }
@@ -12,9 +23,9 @@ module.exports.clear = function () {
 module.exports.setup = function (options) {
 	console.log(name + '::setup')
 	try {
-		exec('git config --global http.proxy ' + options.http)
+		exec('git config --global http.proxy ' + options.http, handler)
 		if (options.https) {
-			exec('git config --global https.proxy ' + options.https)
+			exec('git config --global https.proxy ' + options.https, handler)
 		}
 	} catch (e) {
 	}
