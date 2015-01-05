dadac0f5b4f4b741e0af0010f0e259f1ea9e9adf
Ivan Garavito <ivangaravito@gmail.com>
2015-01-04 21:54:41 -0600

    git: clear command now cleans .gitconfig

--- plugins/git.js
+++ plugins/git.js
@@ -1,5 +1,7 @@
 var name = module.exports.name = 'git'
 	, exec = require('child_process').exec
+	, fs = require('fs')
+	, os = require('os')
 
 var handler = function (error) {
 	if (error !== null) {
@@ -11,11 +13,31 @@ var handler = function (error) {
 	}
 }
 
-module.exports.clear = function () {
+module.exports.clear = function (options) {
 	console.log(name + '::clear')
+
+	var file = options.home + '/.gitconfig'
 	try {
 		exec('git config --global --unset http.proxy', handler)
 		exec('git config --global --unset https.proxy', handler)
+
+		fs.readFile(file, function (err, buffer) {
+			if (err) {
+				console.log('[' + name + '] File "' + file + '" not found.')
+				return
+			}
+
+			var data = buffer.toString()
+			data = data.replace('[http]', '')
+			data = data.replace('[https]', '')
+			data = data.replace(os.EOL + os.EOL, '')
+
+			fs.writeFile(file, data, function (err) {
+				if (err) {
+					console.log('[' + name + '] Cannot write to file "' + file + '".')
+				}
+			})
+		})
 	} catch (e) {
 	}
 }
