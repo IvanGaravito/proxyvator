b947a3ce35dd8129285fbfebb8086b5802e7e802
Ivan Garavito <ivangaravito@gmail.com>
2015-01-07 19:16:20 -0600

    git: fix sync to get .gitconfig cleared

--- plugins/git.js
+++ plugins/git.js
@@ -2,8 +2,11 @@ var name = module.exports.name = 'git'
 	, exec = require('child_process').exec
 	, fs = require('fs')
 	, os = require('os')
+	, path = require('path')
 
-var handler = function (error) {
+var handler, handlerHttp, handlerHttps, handlerFile, file
+
+handler = function (error) {
 	if (error !== null) {
 		if (error.message.match(/git --help/) !== null) {
 			console.log('[' + name + '] Returned error code ' + error.code + ': ' + error.message)
@@ -13,31 +16,42 @@ var handler = function (error) {
 	}
 }
 
+handlerHttp = function (error) {
+	handler(error)
+	exec('git config --global --unset https.proxy', handlerHttps)
+}
+
+handlerHttps = function (error) {
+	handler(error)
+	handlerFile()
+}
+
+handlerFile = function () {	
+	fs.readFile(file, function (err, buffer) {
+		if (err) {
+			console.log('[' + name + '] File "' + file + '" not found.')
+			return
+		}
+
+		var data = buffer.toString()
+		data = data.replace('[http]', '')
+		data = data.replace('[https]', '')
+		data = data.replace(os.EOL + os.EOL, '')
+
+		fs.writeFile(file, data, function (err) {
+			if (err) {
+				console.log('[' + name + '] Cannot write to file "' + file + '".')
+			}
+		})
+	})
+}
+
 module.exports.clear = function (options) {
 	console.log(name + '::clear')
 
-	var file = options.home + '/.gitconfig'
+	file = path.join(options.home, '.gitconfig')
 	try {
-		exec('git config --global --unset http.proxy', handler)
-		exec('git config --global --unset https.proxy', handler)
-
-		fs.readFile(file, function (err, buffer) {
-			if (err) {
-				console.log('[' + name + '] File "' + file + '" not found.')
-				return
-			}
-
-			var data = buffer.toString()
-			data = data.replace('[http]', '')
-			data = data.replace('[https]', '')
-			data = data.replace(os.EOL + os.EOL, '')
-
-			fs.writeFile(file, data, function (err) {
-				if (err) {
-					console.log('[' + name + '] Cannot write to file "' + file + '".')
-				}
-			})
-		})
+		exec('git config --global --unset http.proxy', handlerHttp)
 	} catch (e) {
 	}
 }
