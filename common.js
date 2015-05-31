780e7d8f3c4fcc5d7cb492f73fd5d1fd1c5c3421
IvanGaravito <ivangaravito@gmail.com>
2015-05-31 00:07:46 -0500

    Replace tab char in favor to spaces

--- common.js
+++ common.js
@@ -1,80 +1,80 @@
 var fs = require('fs')
-	, path = require('path')
+  , path = require('path')
 
 var debugMode = false
-	, each = Array.prototype.forEach
-	, helpFn
+  , each = Array.prototype.forEach
+  , helpFn
 
 module.exports.debug = function () {
-	if (debugMode === false) return
+  if (debugMode === false) return
 
-	var args = ['Debug:']
-	each.call(arguments, function (arg) {
-		args.push(arg)
-	})
-	console.log.apply(console, args)
+  var args = ['Debug:']
+  each.call(arguments, function (arg) {
+    args.push(arg)
+  })
+  console.log.apply(console, args)
 }
 
 module.exports.error = function (errno, msg) {
-	console.log('Error: ' + msg)
-	if (helpFn) helpFn()
-	process.exit(errno)
+  console.log('Error: ' + msg)
+  if (helpFn) helpFn()
+  process.exit(errno)
 }
 
 module.exports.loadSettings = function (options) {
-	var settings = {}
-		, keys = ['http', 'https', 'same']
-		, obj = {}
-		, props
-	console.log('Loading settings...')
+  var settings = {}
+    , keys = ['http', 'https', 'same']
+    , obj = {}
+    , props
+  console.log('Loading settings...')
 
-	try {
-		settings = fs.readFileSync(path.join(__dirname, 'settings.json'))
-		settings = JSON.parse(settings)
-		props = Object.getOwnPropertyNames(settings)
-	} catch (e) {
-		return false
-	}
+  try {
+    settings = fs.readFileSync(path.join(__dirname, 'settings.json'))
+    settings = JSON.parse(settings)
+    props = Object.getOwnPropertyNames(settings)
+  } catch (e) {
+    return false
+  }
 
-	if (props === 0) {
-		return false
-	}
+  if (props === 0) {
+    return false
+  }
 
-	keys.forEach(function (key) {
-		obj[key] = settings[key]
-		if (options !== undefined) options[key] = settings[key]
-	})
-	return obj
+  keys.forEach(function (key) {
+    obj[key] = settings[key]
+    if (options !== undefined) options[key] = settings[key]
+  })
+  return obj
 }
 
 module.exports.saveSettings = function (options) {
-	var settings = {}
-		, keys = ['http', 'https', 'same']
-	console.log('Saving settings...')
+  var settings = {}
+    , keys = ['http', 'https', 'same']
+  console.log('Saving settings...')
 
-	keys.forEach(function (key) {
-		settings[key] = options[key]
-	})
-	module.exports.debug('settings=', JSON.stringify(settings))
+  keys.forEach(function (key) {
+    settings[key] = options[key]
+  })
+  module.exports.debug('settings=', JSON.stringify(settings))
 
-	fs.writeFile(path.join(__dirname, 'settings.json'), JSON.stringify(settings), function (err) {
-		if (err) {
-			console.log('Warning: Cannot write settings to settings.json')
-		} else {
-			console.log('Settings saved successfuly')
-		}
-	})
+  fs.writeFile(path.join(__dirname, 'settings.json'), JSON.stringify(settings), function (err) {
+    if (err) {
+      console.log('Warning: Cannot write settings to settings.json')
+    } else {
+      console.log('Settings saved successfuly')
+    }
+  })
 }
 
 module.exports.setDebug = function (debug) {
-	debugMode = !!debug
+  debugMode = !!debug
 }
 
 module.exports.setHelpFn = function (fn) {
-	helpFn = fn
+  helpFn = fn
 }
 
 module.exports.urlValidator = function (theUrl) {
-	var reUrl = /^https?:\/\/(?:(\w+)(?:[:](\w+))?[@])?([A-Za-z0-9-]+(?:[.][A-Za-z0-9-]+)*|(\d{1,3}(?:[.]\d{1,3}){3}))[:](\d+)$/
-	return reUrl.exec(theUrl) === null? false: theUrl
+  var reUrl = /^https?:\/\/(?:(\w+)(?:[:](\w+))?[@])?([A-Za-z0-9-]+(?:[.][A-Za-z0-9-]+)*|(\d{1,3}(?:[.]\d{1,3}){3}))[:](\d+)$/
+  return reUrl.exec(theUrl) === null? false: theUrl
 }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var program = require('commander')
 
 var pkg = require('./package.json')
-	, common = require('./common.js')
-	, plugins = require('./plugins.js')
+  , common = require('./common.js')
+  , plugins = require('./plugins.js')
 
 program
   .version(pkg.version)
@@ -12,75 +12,75 @@ program
   .option('--debug', 'output debbuging messages')
 
 program
-	.command('plugins')
-	.description('list proxyvator plugins installed')
-	.action(function () {
-		plugins.list()
-	})
+  .command('plugins')
+  .description('list proxyvator plugins installed')
+  .action(function () {
+    plugins.list()
+  })
 
 program
-	.command('setup')
-	.description('setup configuration for developing behind the proxy')
+  .command('setup')
+  .description('setup configuration for developing behind the proxy')
   .option('--http <url>', 'set url for http proxy (e.g. http://myproxy.com:8080)', common.urlValidator)
   .option('--https <url>', 'set url for https proxy (e.g. https://me:mypass@myproxy.com:443', common.urlValidator)
   .option('--same', 'use the same http proxy for https')
   .option('--save', 'save settings')
-	.action(function (options) {
-		var self = this
+  .action(function (options) {
+    var self = this
 
-		common.setHelpFn(function () {
-			self.outputHelp()
-		})
-		common.setDebug(options.parent.debug)
+    common.setHelpFn(function () {
+      self.outputHelp()
+    })
+    common.setDebug(options.parent.debug)
 
-		if (options.http === false) common.error(1, 'Bad http proxy url defined')
-		if (options.https === false) common.error(1, 'Bad https proxy url defined')
+    if (options.http === false) common.error(1, 'Bad http proxy url defined')
+    if (options.https === false) common.error(1, 'Bad https proxy url defined')
 
-		if (options.save === true && options.http === undefined && options.https === undefined) common.error(2, 'Cannot save without http or https definition')
-		if (options.http === undefined && options.https === undefined) {
-			var opts = common.loadSettings(options)
-			if (opts === false) common.error(3, 'No configuration saved previously')
-			common.debug('Options loaded:', opts)
-		} else if(options.save) {
-			common.saveSettings(options)
-		}
+    if (options.save === true && options.http === undefined && options.https === undefined) common.error(2, 'Cannot save without http or https definition')
+    if (options.http === undefined && options.https === undefined) {
+      var opts = common.loadSettings(options)
+      if (opts === false) common.error(3, 'No configuration saved previously')
+      common.debug('Options loaded:', opts)
+    } else if(options.save) {
+      common.saveSettings(options)
+    }
 
-		if (options.same === true) {
-			if (options.http !== undefined && options.https === undefined) {
-				options.https = options.http
-			} else {
-				common.error(4, 'Cannot use the same http proxy for https if http is not defined')
-			}
-		}
+    if (options.same === true) {
+      if (options.http !== undefined && options.https === undefined) {
+        options.https = options.http
+      } else {
+        common.error(4, 'Cannot use the same http proxy for https if http is not defined')
+      }
+    }
 
-		options.home = process.env.HOME
-		common.debug('HOME=', options.home)
+    options.home = process.env.HOME
+    common.debug('HOME=', options.home)
 
-		console.log('Setting up...')
-		plugins.setup(options)
-	})
+    console.log('Setting up...')
+    plugins.setup(options)
+  })
 
 program
   .command('clear')
   .description('clear configuration for developing behind the proxy')
-	.action(function (options) {
-		var self = this
+  .action(function (options) {
+    var self = this
 
-		common.setHelpFn(function () {
-			self.outputHelp()
-		})
-		if (options !== undefined) common.setDebug(options.parent.debug)
+    common.setHelpFn(function () {
+      self.outputHelp()
+    })
+    if (options !== undefined) common.setDebug(options.parent.debug)
 
-		options.home = process.env.HOME
-		common.debug('HOME=', options.home)
+    options.home = process.env.HOME
+    common.debug('HOME=', options.home)
 
-		console.log('Clearing...')
-		plugins.clear(options)
-	})
+    console.log('Clearing...')
+    plugins.clear(options)
+  })
 
 program.parse(process.argv)
 
 if (!program.args.length) {
-	program.help()
-	process.exit()
+  program.help()
+  process.exit()
 }
